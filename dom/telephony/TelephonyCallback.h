7ec38daeb21576d3319c9df50e7052b129aa532b
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2014-08-28 13:13:41 +0800

    Dial MMI (callback)

--- dom/telephony/TelephonyCallback.h
+++ dom/telephony/TelephonyCallback.h
@@ -5,12 +5,18 @@
 #ifndef mozilla_dom_TelephonyCallback_h
 #define mozilla_dom_TelephonyCallback_h
 
+#include "mozilla/dom/ToJSValue.h"
 #include "nsCOMPtr.h"
 #include "nsITelephonyService.h"
+#include "nsJSUtils.h"
+#include "nsString.h"
+
+class nsPIDOMWindow;
 
 namespace mozilla {
 namespace dom {
 
+class MMIRequest;
 class Promise;
 class Telephony;
 
@@ -22,15 +28,49 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSITELEPHONYCALLBACK
 
-  TelephonyCallback(Telephony* aTelephony, Promise* aPromise,
-                    uint32_t aServiceId);
+  TelephonyCallback(nsPIDOMWindow* aWindow, Telephony* aTelephony,
+                    Promise* aPromise, uint32_t aServiceId);
+
+  nsresult
+  NotifyDialMMISuccess(const nsAString& aStatusMessage);
+
+  template<typename T>
+  nsresult
+  NotifyDialMMISuccess(const nsAString& aStatusMessage, const T& aInfo)
+  {
+    AutoJSAPI jsapi;
+    if (!NS_WARN_IF(jsapi.Init(mWindow))) {
+      return NS_ERROR_FAILURE;
+    }
+
+    JSContext* cx = jsapi.cx();
+    JS::Rooted<JS::Value> info(cx);
+
+    if (!ToJSValue(cx, aInfo, &info)) {
+      JS_ClearPendingException(cx);
+      return NS_ERROR_TYPE_ERR;
+    }
+
+    return NotifyDialMMISuccess(aStatusMessage, info);
+  }
+
+  nsresult
+  NotifyDialMMISuccess(const nsAString& aStatusMessage,
+                       JS::Handle<JS::Value> aInfo);
+
+  nsresult
+  NotifyDialMMISuccess(const MozMMIResult& aResult);
 
 private:
   ~TelephonyCallback() {}
 
+  nsCOMPtr<nsPIDOMWindow> mWindow;
   nsRefPtr<Telephony> mTelephony;
   nsRefPtr<Promise> mPromise;
   uint32_t mServiceId;
+
+  nsRefPtr<MMIRequest> mMMIRequest;
+  nsString mServiceCode;
 };
 
 } // namespace dom
